0d14d5f590061585a80e8967986647184cac1d78
ThreeDokNait <nikitaaleksandrovichshiyatov@gmail.com>
2023-08-05 16:53:35 +0300

    Коммит для пуша

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿Console.Clear();
+
+// Task 1
+
+// Console.WriteLine("Enter number, please");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int SummF()
+// {
+//     int sum = 0;
+//     int count = 1;
+
+//     while (number >= count)
+//     {
+//         sum = sum+count;
+//         count++;
+//     }
+//     return sum;
+// }
+// Console.WriteLine(SummF());
+
+
+// Task 2
+
+// Console.WriteLine("Enter number, please");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int Qest()
+// {
+//     int count = 0;
+
+//     while (number > 0)
+//     {
+//         count++;
+//         number/= 10;
+//     }
+//     return count;
+// }
+// Console.WriteLine(Qest());
+
+
+// Task 3
+
+// Console.WriteLine("Enter number, please");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int Qest()
+// {
+//     int mult = 1;
+
+//     for (int i = 1; i <= number; i++)
+//     {
+//         mult*=i;
+//     }
+//     return mult;
+// }
+// Console.WriteLine(Qest());
